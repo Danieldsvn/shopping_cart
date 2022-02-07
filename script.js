e9c5d35c4ce0fe9e39c12518a459cffdeb3a2021
Daniel Saraiva <danieldsvn@gmail.com>
2022-02-07 17:33:25 -0300

    requisito 3 concluido

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui  
+function cartItemClickListener(event) {  
+  event.target.remove(); 
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
